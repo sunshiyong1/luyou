21c358783bab4ead7b4fa020666b46aa9f10d221
sunshiyong <1620079535@qq.com>
2020-10-02 20:50:59 +0800

    1

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,29 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <router-link to="/">首页</router-link>
+    <!-- <router-link to="/list?type=动漫">列表</router-link> -->
+    <!-- <router-link v-for="item in types" :key="item" :to="'/list?type=' + item">{{item}}</router-link> -->
+    <!-- <router-link v-for="item in types" :key="item" 
+    :to="{name:'List',query: {type: item}}">{{item}}</router-link> -->
+    <router-link v-for="item in types" :key="item" :to="{name:'List',query: {type:item}}">{{item}}</router-link>
+    <router-view></router-view>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
+  data(){
+    return {
+      types:['动漫','电影','综艺','电视剧']
+    }
+  },
   components: {
-    HelloWorld
+    // HelloWorld
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,22 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router'
+Vue.use(VueRouter)
+
+import Home from './views/Home'
 
 Vue.config.productionTip = false
 
+const router = new VueRouter({
+  routes:[
+    {name:'Home',path:'/',component:Home,},
+    {name:'List',path:'/list',component:() => import('./views/List')}
+  ]
+})
+
+
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
